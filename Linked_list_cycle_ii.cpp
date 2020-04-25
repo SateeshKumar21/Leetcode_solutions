8ebcbdf7933aad504e64e67daabb83aa887f4cb7
SateeshKumar21 <sateeshkarira@gmail.com>
2020-04-25 22:04:56 +0500

    Added 1st script

---
+++ Linked_list_cycle_ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+        unordered_set<ListNode*> node_set;
+        
+        ListNode* ptr = head;
+        
+        while(ptr != NULL){
+            
+            if (node_set.find(ptr) == node_set.end()){
+                
+                node_set.insert(ptr);
+                
+            }
+            else
+                return ptr;
+            ptr = ptr -> next;
+        }
+        
+        return NULL;
+        
+    }
+};
